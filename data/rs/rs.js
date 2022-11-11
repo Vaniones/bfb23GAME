0220fa0f5c7015ec8ffe3edbc35d5f2ccbf7c9d7
Vaniones <112714357+Vaniones@users.noreply.github.com>
2022-11-12 00:47:48 +0300

    Update rs.js

--- data/rs/rs.js
+++ data/rs/rs.js
@@ -17,18 +17,3 @@ const rs = [
 [false,true,false,false,false,false,false,false,false,false,false,0,0,false,false,true,1,false],
 [true,true,true,true,true,false,false,false,false,false,false,0,0,false,false,true,1,false],
 ];
-const rs1 = [
-  [false,false,false,false,false,false,false,false,true,true,false,0,0,false,false,true,0,false],
-[true,true,true,true,false,false,false,false,true,false,false,0,0,false,false,true,14,false,[0,1,2,3,4,5,6,7,8,9,10,11,12,13]],
-[true,true,true,true,false,false,false,false,true,false,false,0,6,false,false,true,12,true,[0,1,2,3,4,5,6,7,8,9,10,11]],
-[false,false,false,false,false,false,false,false,true,false,true,0,0,false,false,true,41,true,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]],
-[true,true,true,true,false,false,false,false,true,false,false,0,6,false,false,true,12,true,[0,1,2,3,4,5,6,7,8,9,10,11]],
-[true,true,true,true,true,true,true,true,false,false,false,0,0,false,false,true,1,true],
-[false,true,false,false,false,false,false,false,false,false,false,0,0,false,false,true,1,false],
-[true,true,true,true,true,false,false,false,false,false,false,0,0,false,false,true,1,false],
-[false,false,false,false,false,false,false,false,true,false,true,0,0,false,false,true,2,true,[0,1,2,3,4,5,6,7,8,9,10,11,12]],
-[true,true,true,true,false,false,false,false,true,false,false,0,6,false,false,true,12,true,[0,1,2,3,4,5,6,7,8,9,10,11]],
-[true,true,true,true,true,true,true,true,false,false,false,0,0,false,false,true,1,true],
-[false,true,false,false,false,false,false,false,false,false,false,0,0,false,false,true,1,false],
-[true,true,true,true,true,false,false,false,false,false,false,0,0,false,false,true,1,false],
-];
