7a1e6ba8cf05bc7566ef8b49d5eff21f6cfb25ff
Vaniones <112714357+Vaniones@users.noreply.github.com>
2022-11-11 20:38:03 +0300

    Update 23.js

--- 23.js
+++ 23.js
@@ -59,6 +59,20 @@ let hoverText = 0
         [false,true,false,false,false,false,false,false,false,false,false,0,0,false,false,true,1,false],
         [true,true,true,true,true,false,false,false,false,false,false,0,0,false,false,true,1,false],
         ];
+	    const tranformProperties = [
+	    	[false,false,false,false,false,false,false,false,false,false,false,false,false,0,0,false,false,false,false,false,false,],
+[true,true,true,true,true,true,true,true,true,true,false,0,0,true,true,true,true,true,true,true,true,true,true,true,true,true,0,1,[0,1,2,3,4,5,6,7,8,9,10,11,12,13]],
+[true,true,true,true,false,false,false,false,false,false,false,0,0,true,false,true,1,false],
+[false,false,false,false,false,false,false,false,false,false,true,0,0,false,false,true,1,false],
+[false,false,false,false,false,false,false,false,true,true,false,0,0,false,false,true,0,false],
+[true,true,true,true,false,false,false,false,true,false,false,0,0,false,false,true,14,false,[0,1,2,3,4,5,6,7,8,9,10,11,12,13]],
+[true,true,true,true,false,false,false,false,true,false,false,0,6,false,false,true,12,true,[0,1,2,3,4,5,6,7,8,9,10,11]],
+[false,false,false,false,false,false,false,false,true,false,true,0,0,false,false,true,41,true,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]],
+[true,true,true,true,false,false,false,false,true,false,false,0,6,false,false,true,12,true,[0,1,2,3,4,5,6,7,8,9,10,11]],
+[true,true,true,true,true,true,true,true,false,false,false,0,0,false,false,true,1,true],
+[false,true,false,false,false,false,false,false,false,false,false,0,0,false,false,true,1,false],
+[true,true,true,true,true,false,false,false,false,false,false,0,0,false,false,true,1,false],
+	    ];
     }
 }
 
@@ -274,3 +288,6 @@ let coins;
 let longMode = false;
 let quirksMode = false;
 let enableExperimentalFeatures = false;
+
+let mdao1 = 0
+let mdao2 = 0
